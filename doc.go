459f119bd4c15cb5126fa5f2005fcd81bc7da962
Dale Lakes <spitfire@spitfy.re>
2019-09-25 21:51:02 -0400

    Add to doc.go

--- doc.go
+++ doc.go
@@ -1,8 +1,24 @@
 /*
 Package gourmet is an extendable network analysis and intrusion detection system.
 
-gourmet contains example analyzers as sub-packages, along with examples of how to use gourmet. If
-you plan to implement your own analyzer or third-party analyzers.
+Gourmet is designed to be fast, simple, and customized. To customize your Gourmet sensor, you can
+implement existing analyzers, or create your own.
+
+The Gourmet Project repository contains analyzers for the following protocols:
+
+  * HTTP (https://github.com/gourmetproject/http_analyzer)
+  * DNS (https://github.com/gourmetprojecct/dns_analyzer)
+
+In order to customize Gourmet, you must customize the project's config.yml file. The default
+contents of the config.yml file are below.
+
+  type: libpcap
+  promiscuous: true
+  interface: eth0
+  snapshot_length: 262144
+  log_file: /log/gourmet.log
+  analyzers:
+    - "github.com/gourmetproject/simple_analyzer"
 
 Usage With No Analyzers
 
@@ -11,6 +27,18 @@ information is contained in a Connection type. This Connection type is marshalle
 and appended to the log file.
 
 For UDP connections, each packet is transformed into a Connection object. However, for TCP
-connections, the stream is first reassembled and then turned into a Connection object. 
+connections, the stream is first reassembled and then turned into a Connection object.
+
+Usage With Analyzers
+
+If you wish to add an analyzer to Gourmet, you must add the GitHub URL to the config.yml file.
+
+Creating Your Own Analyzer
+
+Analyzers are an implementation of the Analyzer interface. They are written as a Go plugin. More
+information about Go plugins can be found here: golang.org/pkg/plugin.
+
+Example custom analyzers can be found in the Gourmet Project repository at
+https://github.com/gourmetproject. simple_analyzer is the best one to start with.
  */
 package gourmet
