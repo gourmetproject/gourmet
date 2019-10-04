73ea5bf57e69e71eb169ef7afdbf71c55e246160
Dale Lakes <spitfire@spitfy.re>
2019-10-03 20:59:01 -0400

    Expose Analyzer configs via GetAnalyzerConfigs function

--- config.go
+++ config.go
@@ -1,13 +1,29 @@
 package gourmet
 
+import (
+	"errors"
+	"fmt"
+)
+
 type Config struct {
 	InterfaceType   string `yaml:"type"`
 	Interface       string
-	Promiscuous     bool	 `yaml:",omitempty"`
+	Promiscuous     bool
 	ConnTimeout     int    `yaml:"connection_timeout"`
 	SnapLen         int    `yaml:"snapshot_length"`
-	Bpf             string `yaml:",omitempty"`
+	Bpf             string
 	LogFile         string `yaml:"log_file"`
-	UpdateAnalyzers bool `yaml:"update_analyzers"`
+	UpdateAnalyzers bool   `yaml:"update_analyzers"`
 	Analyzers       map[string]interface{}
+}
+
+var (
+	analyzerConfigs map[string]interface{}
+)
+
+func GetAnalyzerConfig(key string) (interface{}, error) {
+	val, ok := analyzerConfigs[key]; if !ok {
+		return nil, errors.New(fmt.Sprintf("analyzer %s does not exist", key))
+	}
+	return val, nil
 }
